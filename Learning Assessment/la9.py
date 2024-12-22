bcd392fe3f850b8fc15a162ddcbd98f3b952202f
justinpalo <markjustineppo.somine@fcpc-inc.com>
2024-12-22 20:46:49 +0800

    Add files via upload

---
+++ Learning Assessment/la9.py
@@ -0,0 +1,10 @@
+class Car: 
+    def __init__(self, brand):
+        self.brand = brand
+    def __self__(self):
+        return f" My fucking car is a {self.brand}, high flyer in the universe"
+
+my_car = "kidlat"
+print(my_car)
+del my_car
+print(my_car)        
