f673302e3101b66e7a5e93ba04f7fa9687ee6ae9
justinpalo <markjustineppo.somine@fcpc-inc.com>
2024-12-22 20:42:00 +0800

    Add files via upload

---
+++ Learning Assessment/la1.py
@@ -0,0 +1,9 @@
+class ClassSection:
+    name = "Jutsin Somine"
+    section = "BSIT 2B"
+
+username = ClassSection()
+section2 = ClassSection()
+
+print(f"My name is {username.name}")
+print(section2.section)
